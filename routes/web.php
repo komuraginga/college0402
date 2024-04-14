faa1a707c92bc90c2e84e6fff47c033fa55ce262
komuraginga <ec2-user@ip-172-31-88-198.ec2.internal>
2024-04-14 13:12:33 +0900

    third commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+<<<<<<< HEAD
+=======
+use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
+>>>>>>> origin/master
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+<<<<<<< HEAD
+=======
+Route::get('/posts', [PostController::class, 'index']);
+>>>>>>> origin/master
