d30bba4dada7089ffdcc3652adf8ca360c211245
komuraginga <ec2-user@ip-172-31-88-198.ec2.internal>
2024-04-23 12:25:08 +0900

    08-3 kadai3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,22 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-<<<<<<< HEAD
 
-class PostController extends Controller
-{
-    //
-=======
-//use宣言は外部にあるクラスをPostController内にインポートできる。
 //この場合、App\Models内のPostクラスをインポートしている。
 use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+    public function index(Post $post)
     {
-    return $post->get();//$postの中身を戻り値にする。
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
     }
-
->>>>>>> origin/master
+       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+       
+       /**
+ * 特定IDのpostを表示する
+ *
+ * @params Object Post // 引数の$postはid=1のPostインスタンス
+ * @return Reposnse post view
+ */
+public function show(Post $post)
+{
+    return view('posts/show')->with(['post' => $post]);
+ //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
 }
+}
+
+?>    
