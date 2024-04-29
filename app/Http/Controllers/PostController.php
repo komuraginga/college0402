3bd1475c1b0df4965c8ab127b5e20a17afb243d4
komuraginga <ec2-user@ip-172-31-88-198.ec2.internal>
2024-04-29 15:28:44 +0900

    0429

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,15 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-//この場合、App\Models内のPostクラスをインポートしている。
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
     }
        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
        
@@ -25,6 +24,31 @@ public function show(Post $post)
 {
     return view('posts/show')->with(['post' => $post]);
  //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+}
+
+ public function create()
+{
+return view('posts/create');
+}
+
+public function store(Request $request, Post $post)
+{
+    $input = $request['post'];
+    $post->fill($input)->save();
+    return redirect('/posts/' . $post->id);
+}
+
+public function edit(Post $post)
+{
+    return view('posts.edit')->with(['post' => $post]);
+}
+
+ public function update(PostRequest $request, Post $post)
+{
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+
+    return redirect('/posts/' . $post->id);
 }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,9 @@ use App\Http\Controllers\PostController;  //外部にあるPostControllerクラ�
 */
 
 Route::get('/', [PostController::class, 'index']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
-// '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
+// '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行す
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post}', [PostController::class, 'update']);
